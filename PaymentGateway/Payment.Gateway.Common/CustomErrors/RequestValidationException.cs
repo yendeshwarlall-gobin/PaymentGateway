fac0cf322c6a7c2fe794e1761fd9d9b2b0008c71
yendeshwarlall-gobin <gobinyesley@gmail.com>
2020-05-31 16:11:59 +0400

    Payment Gateway - Common

---
+++ PaymentGateway/Payment.Gateway.Common/CustomErrors/RequestValidationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Payment.Gateway.Common.CustomErrors
+{
+    public class RequestValidationException : Exception
+    {
+        public RequestValidationException(string message) : base(message)
+        {
+
+        }
+    }
+}
